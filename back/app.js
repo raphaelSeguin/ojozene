6feb5ce2ec558a5311c3152c2a9b5b46feb01a3e
raphaelSeguin <raphaelseguin@gmail.com>
2019-09-12 10:12:02 +0200

    enforce https in production

--- back/app.js
+++ back/app.js
@@ -44,7 +44,7 @@ if (process.env.NODE_ENV === 'production') {
 
     // enforce HTTPS
 
-    app.use('*', (req, res, next) => {
+    app.use( (req, res, next) => {
         if (req.protocol !== 'https') {
             // res.redirect('https://' + req.hostname + req.url);
             return res.redirect(['https://', req.get('Host'), req.baseUrl].join(''))
